a5d4b74d174d49cea8e78323dd997c383aaefd73
tttMelody <245915794@qq.com>
2018-04-03 21:17:44 +0800

    c

--- src/main/java/dict/DictTest.java
+++ src/main/java/dict/DictTest.java
@@ -2,11 +2,14 @@ package dict;
 
 import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.apache.commons.io.output.ByteArrayOutputStream;
@@ -14,22 +17,22 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
 
 public class DictTest {
-    private int CAP = 1000;
+    private int CAP = 1000 * 1000;
 
-    @Test
-    public void write() throws IOException {
+    public void write(String dictName) throws IOException {
         String[] dict = new String[CAP];
         for (int i = 0; i < dict.length; i++) {
             dict[i] = gen();
         }
-        writeDict(dict);
+        writeDict(dict, dictName);
     }
 
     @Test
     public void read() throws IOException {
-        write();
-        File file = new File("dict");
+        String dictName = "dict";
+        write(dictName);
 
+        File file = new File(dictName);
         try (RandomAccessFile r = new RandomAccessFile(file, "r")) {
             int len = r.readInt();
 
@@ -39,14 +42,62 @@ public class DictTest {
             }
 
             long t1 = System.currentTimeMillis();
-            for (int i = 0; i < 1000000; i++) {
+            for (int i = 0; i < CAP; i++) {
                 get(r, pos, new Random().nextInt(CAP));
             }
             System.out.println("duration:" + (System.currentTimeMillis() - t1));
-        } catch (IOException e) {
-            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testSense() throws IOException {
+//        simulation(1, 1_000_000); // baseline duration:3621
+//        simulation(1, 10_000); // baseline duration:3487
+//        simulation(3, 10_000); // duration:10746
+//        simulation(3, 10_0000); // duration:10579
+//        simulation(6, 10_000); // duration:24899
+//        simulation(10, 10_000); // duration:48514
+//        simulation(20, 10_000); // duration:103165
+//        simulation(30, 10_000); // duration:146469
+     }
+
+    public void simulation(int cols, int readOnetime) throws IOException {
+        List<RandomAccessFile> ins = new ArrayList<>(cols);
+        for (int i = 0; i < cols; i++) {
+            String dictName = "dict" + i;
+            write(dictName);
+            ins.add(getInputStream(dictName));
         }
 
+        List<int[]> headers = new ArrayList<>(cols);
+
+        for (int i = 0; i < cols; i++) {
+            headers.add(getHeader(ins.get(i)));
+        }
+
+        long t1 = System.currentTimeMillis();
+        for (int i = 0; i < CAP / readOnetime; i++) { // rounds
+            for (int k = 0; k < cols; k++) { // simulation n cols
+                for (int j = 0; j < readOnetime; j++) { // n rows per col and round
+                    get(ins.get(k), headers.get(k), new Random().nextInt(CAP));
+                }
+            }
+        }
+        System.out.println("duration:" + (System.currentTimeMillis() - t1));
+    }
+
+    private int[] getHeader(RandomAccessFile in) throws IOException {
+        int len = in.readInt();
+        int[] pos = new int[len];
+        for (int i = 0; i < pos.length; i++) {
+            pos[i] = in.readInt();
+        }
+        return pos;
+    }
+
+    private RandomAccessFile getInputStream(String dictName) throws FileNotFoundException {
+        File file = new File(dictName);
+        return new RandomAccessFile(file, "r");
     }
 
     private static void printOri(String[] dict) {
@@ -58,14 +109,14 @@ public class DictTest {
         }
     }
 
-    private static void writeDict(String[] dict) throws IOException {
+    private static void writeDict(String[] dict, String name) throws IOException {
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         DictHeader header = new DictHeader(dict);
         DictValue value = new DictValue(dict);
         DictHeader.DictHeaderSerializer.DICT_HEADER_SERIALIZER.serialize(header, new DataOutputStream(out));
         DictValue.DictValueSerializer.DICT_VALUE_SERIALIZER.serialize(value, new DataOutputStream(out));
 
-        try (OutputStream outputStream = new FileOutputStream("dict")) {
+        try (OutputStream outputStream = new FileOutputStream(name)) {
             out.writeTo(outputStream);
             out.close();
         }
